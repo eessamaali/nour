0d75d4fd95095e5f369d13901d4cbde9dc5d6879
Mohamed Essamaali <eessamaali996@gmail.com>
2022-09-08 23:21:19 -0400

    first view nour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nour';
+  images:any;
+  imgs:any;
+  index=0;
+  moveRight(){
+    if(this.index==this.images.length-1){
+      this.index=0;
+    } else{
+      this.index = this.index+1;
+    }
+      
+  }
+  moveLeft(){
+    if(this.index==0){
+      this.index=this.images.length-1;
+    } else{
+      this.index = this.index-1;
+    }
+      }
+  ngOnInit(): void {
+
+    setInterval((e:any)=>{
+      this.moveRight()
+    },3000)
+    
+    this.images =
+    [    
+      '../assets/p6.jpg',
+      '../assets/p1.jpg',
+      '../assets/p2.jpg',
+      '../assets/p3.jpg',
+      '../assets/p4.jpg',
+      '../assets/p5.jpg',
+]
+    
+  }
 }
